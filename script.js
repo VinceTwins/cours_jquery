6dfd8a5bb3866f7e81ea0495353a7698d7593603
VinceTwins <58826083+VinceTwins@users.noreply.github.com>
2020-03-29 17:26:46 +0200

    animation part

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+/*on peut ecrire la même chose en plus court :
+$(function() {
+   
+});*/
+
+/*$(document).ready(function() {
+    $("#start").click(function() {
+        $(".square").animate({
+            left: "600px",
+            opacity: 0.5,
+            width: "200px",
+            height: "200px",
+        }, 5000);
+    });
+    
+    $("#stop").click(function() {
+        $(".square").stop();
+    });
+   
+
+
+});*/
+
 $(document).ready(function() {
-    $("h1").hide();
+    $("#start").click(function() {
+        $(".square").slideUp(3000).slideDown(3000);
+    });
 });
